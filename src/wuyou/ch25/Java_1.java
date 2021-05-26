7f889c33bd85b9ed211fc4e070faf309ee70436a
heiye5 <2716634948@qq.com>
2021-05-26 19:50:37 +0800

    ch25 ch26

---
+++ src/wuyou/ch25/Java_1.java
@@ -0,0 +1,15 @@
+package wuyou.ch25;
+
+// 阅读下列代码：
+public class Java_1
+{
+   //*********Found**********
+   public static void main(String []args)
+   {
+      String s1=new String("你正在考试");
+      String s2=new String("你正在考试");
+      System.out.println(s1==s2);
+      //*********Found**********
+      System.out.println(s1.equals(s2));
+   }
+}
